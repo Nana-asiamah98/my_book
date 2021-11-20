08493faa86d4ebaee5a895d537b822387c1a1a34
Jude <jude@thestudio.com>
2021-11-20 14:30:33 +0000

    Updated index

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ app.use('/api',UserRouter);
 mongoose.connect(MONGO_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => app.listen(PORT,() => {
-    console.log(`Server is running on Port ${PORT}`)
-})).catch((error) => console.log(`${error} did not connect`));
+}).then(() => {
+    console.log("Connected")
+})
+.catch((error) => console.log(`${error} did not connect`));
+
+
+app.listen(()=>{
+    console.log('APP started');
+})
