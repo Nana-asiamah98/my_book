0ee56e9e1f226fcb7c0f2131030e183342c42bed
Jude <jude@thestudio.com>
2021-11-20 14:45:34 +0000

    refactored

--- index.js
+++ index.js
@@ -27,15 +27,9 @@ mongoose.connect(MONGO_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
-    console.log("Connected")
-})
-.catch((error) => console.log(`${error} did not connect`));
+    console.log(`Port -> ${PORT}`)
+}).catch((error) => console.log(`${error} did not connect`));
 
-
-console.log(`MONGO : ${MONGO_URI}`)
-
-app.set(`port`,PORT);
-
-app.listen(()=>{
-    console.log('APP started');
+app.listen(PORT,() => {
+    console.log(`Server is running on Port ${PORT}`)
 })
